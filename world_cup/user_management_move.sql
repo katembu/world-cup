16bccb0c3b8f5966c612e9687a9261de86a3f263
Eric Saupe <ericsaupe@gmail.com>
2013-12-13 18:14:34 -0700

    Update user_management_move.sql

--- world_cup/user_management_move.sql
+++ world_cup/user_management_move.sql
@@ -1,3 +1,3 @@
-INSERT INTO user_management_customuser (id, password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
+INSERT INTO world_cup.user_management_customuser (id, password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
 SELECT id, password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined
-FROM auth_user
+FROM world_cup.auth_user
