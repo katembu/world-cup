1da7c3411b4d529d31307f30d95bc048f094064a
Eric Saupe <ericsaupe@gmail.com>
2013-12-16 13:46:55 -0700

    Update settings.py

--- world_cup/world_cup/settings.py
+++ world_cup/world_cup/settings.py
@@ -107,7 +107,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'world_cup.middleware.ErrorMiddleware',
+    #'world_cup.middleware.ErrorMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
