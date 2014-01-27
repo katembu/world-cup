e7631eb696d4cc5f910897e4b985a16915e3eeb1
Eric Saupe <eric.saupe@idfl.com>
2014-01-27 16:55:27 -0700

    Fixed settings file.  Change donate page a little.

--- world_cup/world_cup/settings.py
+++ world_cup/world_cup/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'world_cup',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': 'R@came8r',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
         'OPTIONS': {
