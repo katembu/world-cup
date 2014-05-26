d33d368034749c3aae40033ecc7c055bcbb180ca
Moses Katembu <mose.kate@gmail.com>
2014-05-26 16:53:25 +0300

    change data for countries

--- world_cup/tournament_countries.sql
+++ world_cup/tournament_countries.sql
@@ -4,35 +4,37 @@ LIMIT 0, 1000
 
 -- Date: 2013-11-19 20:34
 */
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (1,'Brazil','A',1,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (2,'Japan','A',2,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (3,'Australia','A',3,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (4,'Iran','A',4,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (5,'South Korea','B',1,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (6,'Netherlands','B',2,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (7,'Italy','B',3,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (8,'Costa Rica','B',4,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (9,'United States','C',1,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (10,'Argentina','C',2,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (11,'Belgium','C',3,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (12,'Switzerland','C',4,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (13,'Germany','D',1,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (14,'Colombia','D',2,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (15,'Russia','D',3,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (16,'Bosnia and Herzegovina','D',4,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (17,'England','E',1,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (18,'Spain','E',2,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (19,'Chile','E',3,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (20,'Ecuador','E',4,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (21,'Honduras','F',1,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (22,'Greece','F',2,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (23,'France','F',3,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (24,'Portugal','F',4,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (25,'Algeria','G',1,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (26,'Sweden','G',2,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (27,'Iceland','G',3,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (28,'Romania','G',4,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (29,'Croatia','H',1,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (30,'Ghana','H',2,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (31,'Mexico','H',3,NULL);
-INSERT INTO `tournament_countries` (`id`,`name`,`group`,`position`,`final_position`) VALUES (32,'Finland','H',4,NULL);
+INSERT INTO `tournament_countries` (`id`, `name`, `group`, `position`, `final_position`, `world_rank`)
+VALUES
+	(1,'Brazil','A',1,NULL,1),
+	(2,'Japan','A',2,NULL,2),
+	(3,'Australia','A',3,NULL,3),
+	(4,'Iran','A',4,NULL,4),
+	(5,'South Korea','B',1,NULL,5),
+	(6,'Netherlands','B',2,NULL,6),
+	(7,'Italy','B',3,NULL,7),
+	(8,'Costa Rica','B',4,NULL,8),
+	(9,'United States','C',1,NULL,9),
+	(10,'Argentina','C',2,NULL,10),
+	(11,'Belgium','C',3,NULL,11),
+	(12,'Switzerland','C',4,NULL,12),
+	(13,'Germany','D',1,NULL,13),
+	(14,'Colombia','D',2,NULL,14),
+	(15,'Russia','D',3,NULL,15),
+	(16,'Bosnia and Herzegovina','D',4,NULL,16),
+	(17,'England','E',1,NULL,17),
+	(18,'Spain','E',2,NULL,18),
+	(19,'Chile','E',3,NULL,19),
+	(20,'Ecuador','E',4,NULL,20),
+	(21,'Honduras','F',1,NULL,21),
+	(22,'Greece','F',2,NULL,22),
+	(23,'France','F',3,NULL,23),
+	(24,'Portugal','F',4,NULL,24),
+	(25,'Algeria','G',1,NULL,25),
+	(26,'Sweden','G',2,NULL,26),
+	(27,'Iceland','G',3,NULL,27),
+	(28,'Romania','G',4,NULL,28),
+	(29,'Croatia','H',1,NULL,29),
+	(30,'Ghana','H',2,NULL,30),
+	(31,'Mexico','H',3,NULL,31),
+	(32,'Finland','H',4,NULL,32);
